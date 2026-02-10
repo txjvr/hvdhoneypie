74b06dc64d64c20cda6365a54fa1b98723ab5e43
txjvr <summerbelle0748@gmail.com>
2026-02-10 15:21:07 +0300

    Update config.js

--- config.js
+++ config.js
@@ -49,9 +49,9 @@ const CONFIG = {
 
     // Messages that appear after they say "Yes!"
     celebration: {
-        title: "letter from my hert 💓",
+        title: "letter from my heart",
         message: "hi, honey! i thank God we met each other. thank you for always making me feel loved. i will always support you in pursuing what you want- whether it be making the biggest corporate or biggest muskel in the world.",
-        emojis: "💖💝💋❤️💕"  // These will bounce around
+        emojis: "💋❤️"  // These will bounce around
     },
 
     // Color scheme for the website
@@ -67,7 +67,7 @@ const CONFIG = {
     // Animation settings
     // Adjust these if you want faster/slower animations
     animations: {
-        floatDuration: "15s",           // How long it takes hearts to float up (10-20s recommended)
+        floatDuration: "20s",           // How long it takes hearts to float up (10-20s recommended)
         floatDistance: "50px",          // How far hearts move sideways (30-70px recommended)
         bounceSpeed: "0.5s",            // Speed of bouncing animations (0.3-0.7s recommended)
         heartExplosionSize: 1.5         // Size of heart explosion effect (1.2-2.0 recommended)
